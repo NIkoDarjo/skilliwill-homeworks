c2d2aba141d4710c32f6b5a936c777b8519fe4e3
Nikoloz Darjania <nikodarjo@Nikolozs-MacBook-Air.local>
2026-05-05 00:13:36 +0400

    first homework

---
+++ skill will davaleba/main.js
@@ -0,0 +1,42 @@
+
+// // davaleba 1
+// function calculate(a,b){
+//     if(a===b){
+//         console.log("tolia")
+//     }else{
+//         console.log("ar aris toli")
+//     }
+// }
+// calculate(1,1) 
+// calculate(1,2)
+// // davaleba 2
+// function calculate(a){
+//     if(
+//        typeof a === "number"
+//     ){
+//         console.log((a-32)*5/9)
+//     }else{
+//         console.log(false)
+//     }
+// }
+// calculate("34")
+// calculate(34)
+// // davaleba 3
+// function calculate(a, b, operation) {
+//     if (typeof a === "number" && typeof b === "number") {
+//         if(operation === "+"){
+//             console.log(a+b)
+//         }else if(operation === "-"){
+//             console.log(a-b)
+//         }else if(operation === "*"){
+//             console.log(a*b)
+//         }else if(operation === "/"){
+//             console.log(a/b)
+//         }else{
+//             console.log(false)
+//         }
+//     }
+// }
+
+// calculate(7, 4, "*")
+// calculate(7, 4, "**")
