625dea8d4cd0e6ecbceaf825a887e28537bf33c7
Nikoloz Darjania <nikodarjo@Nikolozs-MacBook-Air.local>
2026-05-18 14:04:20 +0400

    homework 4

---
+++ skill will favaleba 4/main.js
@@ -0,0 +1,35 @@
+// // davaleba 1
+
+
+// const replace = (
+//     string, valueToReplace,valueToReplaceWith
+//     ) => {
+//        const x = string.split(valueToReplace)
+//        const y = x.join(valueToReplaceWith)
+
+//        return y}
+
+// console.log(replace("nikoloz darjania","darjani","gabedav"))
+
+
+// // davaleba 2
+
+// const replace = (string) => {
+//       return string.split(" ")
+//       .map((s) => s[0].toUpperCase() + s.slice(1))
+//       .join(" ")
+
+
+// }
+// console.log(replace("nikoloz darjania"))
+
+// // davaleba 3
+
+// const user = [{name: "Lasha", age: 30}, {name: "Saba", age:
+// 20}]
+// const sortbyage = () => {
+//      return user.sort(
+//             (a,b) => a.age-b.age
+//       )
+// }
+// console.log(sortbyage())
